9a9e67ddc70e06c089dc8b12b997b76a2450f6ec
Natalia Epifanova <epif97@mail.ru>
2025-07-18 11:26:18 +0300

    add login and logout

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from restaurant.models import Table
+
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    """
+    Административный интерфейс для модели Table.
+
+    Attributes:
+        list_display (tuple): Поля, отображаемые в списке объектов.
+        search_fields (tuple): Поля, по которым выполняется поиск.
+    """
+
+    list_display = ("table_number", "status", "number_of_seats", "description")
+    search_fields = ("table_number", "number_of_seats")
+
--- users/views.py
+++ users/views.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from users.forms import UserRegisterForm
+from users.models import User
+
+
+class CustomLoginView(LoginView):
+    """
+    Кастомное представление для входа с проверкой блокировки.
+    Attributes:
+        template_name (str): Путь к шаблону страницы входа.
+    """
+
+    template_name = "users/login.html"
+
+class UserCreateView(CreateView):
+    """
+    Представление для регистрации нового пользователя с подтверждением по email.
+    Attributes:
+        model (User): Модель пользователя.
+        form_class (Form): Класс формы UserRegisterForm.
+        success_url (str): URL для перенаправления после успешной регистрации.
+    """
+
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy("users:login")
+
